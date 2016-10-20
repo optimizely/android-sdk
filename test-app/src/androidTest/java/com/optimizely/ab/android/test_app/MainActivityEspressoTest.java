38a4966c2bb4ffd1a48ebc4fac8e4ecfc72c9aa6
Josh Deffibaugh <josh@optimizely.com>
2016-10-19 17:23:15 -0700

    Sets up espresso tests for MainActivity

---
+++ test-app/src/androidTest/java/com/optimizely/ab/android/test_app/MainActivityEspressoTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2016, Optimizely
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.optimizely.ab.android.test_app;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.idling.CountingIdlingResource;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityEspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+    private Context context;
+    private CountingIdlingResource countingIdlingResource;
+
+    @Before
+    public void setupStorage() {
+        // Clear sticky bucketing
+        context = InstrumentationRegistry.getTargetContext();
+        context.deleteFile("optly-user-experiment-record-7664231436.json");
+        // Set the user's id to the test user that is in the whitelist.
+        context.getSharedPreferences("user", Context.MODE_PRIVATE).edit()
+                .putString("userId", "test_user")
+                .apply();
+    }
+
+
+    @Before
+    public void registerIdlingResource() {
+        countingIdlingResource = activityRule.getActivity().getIdlingResource();
+        // To prove that the test fails, omit this call:
+        Espresso.registerIdlingResources(countingIdlingResource);
+    }
+
+    @Test
+    public void experimentActivationForWhitelistUser() {
+        // Check that the text was changed.
+        // These tests are pointed at a real project.
+        // The user 'test_user` is in the whitelist for variation 1 for experiment 0 and experiment 1.
+        onView(withId(R.id.button_1))
+                .check(matches(withText(context.getString(R.string.button_1_text_var_1))));
+
+
+        // Espresso will wait for Optimizely to start due to the registered idling resources
+        onView(withId(R.id.text_view_1))
+                .check(matches(withText(context.getString(R.string.text_view_1_var_1))));
+    }
+
+    @After
+    public void unregisterIdlingResource() {
+        if (countingIdlingResource != null) {
+            Espresso.unregisterIdlingResources(countingIdlingResource);
+        }
+    }
+}
--- test-app/src/main/java/com/optimizely/ab/android/test_app/MainActivity.java
+++ test-app/src/main/java/com/optimizely/ab/android/test_app/MainActivity.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2016, Optimizely
  * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,6 +17,11 @@ package com.optimizely.ab.android.test_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.idling.CountingIdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -31,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
     private OptimizelyManager optimizelyManager;
     private MyApplication myApplication;
+
+
+    // The Idling Resource which will be null in production.
+    @Nullable private CountingIdlingResource countingIdlingResource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +83,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
+        if (countingIdlingResource != null) {
+            countingIdlingResource.increment();
+        }
         optimizelyManager.start(this, new OptimizelyStartListener() {
             @Override
             public void onStart(AndroidOptimizely optimizely) {
@@ -89,7 +101,22 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     textView1.setText(R.string.text_view_1_default);
                 }
+                if (countingIdlingResource != null) {
+                    countingIdlingResource.decrement();
+                }
             }
         });
+    }
+
+    /**
+     * Only called from test, creates and returns a new {@link CountingIdlingResource}.
+     */
+    @VisibleForTesting
+    @NonNull
+    public CountingIdlingResource getIdlingResource() {
+        if (countingIdlingResource == null) {
+            countingIdlingResource = new CountingIdlingResource("onOptimizelyStart", true);
+        }
+        return countingIdlingResource;
     }
 }
