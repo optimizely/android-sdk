0b8289a9781846bb823550b4c05a86151ad26bdd
Mike Ng <mike.ng@optimizely.com>
2016-11-22 15:54:36 -0800

    Make class visible for testing to address failure in travis.

--- android-sdk/src/main/java/com/optimizely/ab/android/sdk/DataFileCache.java
+++ android-sdk/src/main/java/com/optimizely/ab/android/sdk/DataFileCache.java
@@ -18,6 +18,7 @@ package com.optimizely.ab.android.sdk;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
 
 import com.optimizely.ab.android.shared.Cache;
 
@@ -31,6 +32,7 @@ import java.io.IOException;
 /*
  * Abstracts the actual data "file" {@link java.io.File}
  */
+@VisibleForTesting
 class DataFileCache {
 
     private static final String OPTLY_DATA_FILE_NAME = "optly-data-file-%s.json";
