8462ecb3429177de4dba677f9244dceb18026ff9
Vignesh Raja <vignesh@optimizely.com>
2017-01-20 14:44:34 -0800

    Revert "Update copyright (#68)"
    This reverts commit 3c7a81c00604fd791653dda265c8317fe427617a.

--- event-handler/src/main/java/com/optimizely/ab/android/event_handler/EventClient.java
+++ event-handler/src/main/java/com/optimizely/ab/android/event_handler/EventClient.java
@@ -1,18 +1,18 @@
-/****************************************************************************
- * Copyright 2016, Optimizely, Inc. and contributors                        *
- *                                                                          *
- * Licensed under the Apache License, Version 2.0 (the "License");          *
- * you may not use this file except in compliance with the License.         *
- * You may obtain a copy of the License at                                  *
- *                                                                          *
- *    http://www.apache.org/licenses/LICENSE-2.0                            *
- *                                                                          *
- * Unless required by applicable law or agreed to in writing, software      *
- * distributed under the License is distributed on an "AS IS" BASIS,        *
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
- * See the License for the specific language governing permissions and      *
- * limitations under the License.                                           *
- ***************************************************************************/
+/*
+ * Copyright 2016, Optimizely
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.optimizely.ab.android.event_handler;
 
@@ -26,7 +26,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 
-/**
+/*
+ * Created by jdeffibaugh on 7/21/16 for Optimizely.
+ * <p>
  * Makes network requests related to events
  */
 class EventClient {
