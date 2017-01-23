17c19493d8c12948eedc432e921c40a078ee7411
Elliot Kim <elliot@optimizely.com>
2017-01-23 17:02:08 +0100

    Update copyright

--- android-sdk/src/main/java/com/optimizely/ab/android/sdk/DataFileCache.java
+++ android-sdk/src/main/java/com/optimizely/ab/android/sdk/DataFileCache.java
@@ -1,24 +1,23 @@
-/*
- * Copyright 2016, Optimizely
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/****************************************************************************
+ * Copyright 2016, Optimizely, Inc. and contributors                        *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ *    http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ***************************************************************************/
 
 package com.optimizely.ab.android.sdk;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.VisibleForTesting;
 
 import com.optimizely.ab.android.shared.Cache;
 
@@ -26,10 +25,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-/*
+/**
  * Abstracts the actual data "file" {@link java.io.File}
  */
 public class DataFileCache {
@@ -73,7 +69,6 @@ public class DataFileCache {
     boolean save(String dataFile) {
         return cache.save(getFileName(), dataFile);
     }
-
 
     String getFileName() {
         return String.format(OPTLY_DATA_FILE_NAME, projectId);
