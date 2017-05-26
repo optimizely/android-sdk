628df30488ca56e4e7a1aa876a9af2231fb4a10f
Thomas Zurkan <thomas.zurkan@optimizely.com>
2017-05-26 13:35:03 -0700

    remove unused imports

--- android-sdk/src/main/java/com/optimizely/ab/android/sdk/OptimizelyManager.java
+++ android-sdk/src/main/java/com/optimizely/ab/android/sdk/OptimizelyManager.java
@@ -53,8 +53,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
