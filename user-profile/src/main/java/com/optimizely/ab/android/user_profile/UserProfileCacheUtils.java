5d1dfd5008a993156a7fb1a1e12f856c1dae7437
Thomas Zurkan <thomas.zurkan@optimizely.com>
2017-07-17 15:54:07 -0700

    fix silly misspellings..

--- user-profile/src/main/java/com/optimizely/ab/android/user_profile/UserProfileCacheUtils.java
+++ user-profile/src/main/java/com/optimizely/ab/android/user_profile/UserProfileCacheUtils.java
@@ -37,7 +37,7 @@ public class UserProfileCacheUtils {
      *
      * @param userProfilesJson JSONObject representing the user profile from UserProfileService
      * @return map of user id key and experiments with variations the user has participated in.
-     * @throws JSONException Exception if the json problem
+     * @throws JSONException Exception if there is a json parsing problem
      */
     public static Map<String, Map<String, Object>> convertJSONObjectToMap(JSONObject userProfilesJson) throws
             JSONException {
@@ -75,9 +75,9 @@ public class UserProfileCacheUtils {
     /**
      * Transform a user profile map to a user profile JSONObject.
      *
-     * @param userProfilesMap map with user id as key and experiements varition map from there.
-     * @return jsonobjec of the user profile service
-     * @throws Exception if json is malformed or map is malformed, an exception can occur.
+     * @param userProfilesMap map with user id as key and experiments variation map from there.
+     * @return JSONObject of the user profile service
+     * @throws Exception if the json is malformed or the map is malformed, an exception can occur.
      */
     public static JSONObject convertMapToJSONObject(Map<String, Map<String, Object>> userProfilesMap) throws Exception {
         JSONObject userProfilesJson = new JSONObject();
