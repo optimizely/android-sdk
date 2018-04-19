04b0ccea3255b0f429278a51b510260594ffa548
Thomas Zurkan <thomas.zurkan@optimizely.com>
2018-04-19 11:17:58 -0700

    fix for blue apron by providing default logger if none is supplied.

--- android-sdk/src/main/java/com/optimizely/ab/android/sdk/OptimizelyManager.java
+++ android-sdk/src/main/java/com/optimizely/ab/android/sdk/OptimizelyManager.java
@@ -45,6 +45,7 @@ import com.optimizely.ab.event.internal.payload.EventBatch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -628,7 +629,323 @@ public class OptimizelyManager {
                 try {
                     logger = LoggerFactory.getLogger(OptimizelyManager.class);
                 } catch (Exception e) {
-                    logger = LoggerFactory.getLogger("com.optimizely.ab.android.sdk.OptimizelyManager");
+                    try {
+                        logger = LoggerFactory.getLogger("com.optimizely.ab.android.sdk.OptimizelyManager");
+                    }
+                    catch (Exception e1) {
+                        logger = new Logger() {
+                            @Override
+                            public String getName() {
+
+                                return "NoopLogger";
+                            }
+
+                            @Override
+                            public boolean isTraceEnabled() {
+                                return false;
+                            }
+
+                            @Override
+                            public void trace(String msg) {
+
+                            }
+
+                            @Override
+                            public void trace(String format, Object arg) {
+
+                            }
+
+                            @Override
+                            public void trace(String format, Object arg1, Object arg2) {
+
+                            }
+
+                            @Override
+                            public void trace(String format, Object... arguments) {
+
+                            }
+
+                            @Override
+                            public void trace(String msg, Throwable t) {
+
+                            }
+
+                            @Override
+                            public boolean isTraceEnabled(Marker marker) {
+                                return false;
+                            }
+
+                            @Override
+                            public void trace(Marker marker, String msg) {
+
+                            }
+
+                            @Override
+                            public void trace(Marker marker, String format, Object arg) {
+
+                            }
+
+                            @Override
+                            public void trace(Marker marker, String format, Object arg1, Object arg2) {
+
+                            }
+
+                            @Override
+                            public void trace(Marker marker, String format, Object... argArray) {
+
+                            }
+
+                            @Override
+                            public void trace(Marker marker, String msg, Throwable t) {
+
+                            }
+
+                            @Override
+                            public boolean isDebugEnabled() {
+                                return false;
+                            }
+
+                            @Override
+                            public void debug(String msg) {
+                                if (BuildConfig.DEBUG) {
+                                    System.out.println("DEBUG:" + msg);
+                                }
+
+                            }
+
+                            @Override
+                            public void debug(String format, Object arg) {
+
+                            }
+
+                            @Override
+                            public void debug(String format, Object arg1, Object arg2) {
+
+                            }
+
+                            @Override
+                            public void debug(String format, Object... arguments) {
+
+                            }
+
+                            @Override
+                            public void debug(String msg, Throwable t) {
+
+                            }
+
+                            @Override
+                            public boolean isDebugEnabled(Marker marker) {
+                                return false;
+                            }
+
+                            @Override
+                            public void debug(Marker marker, String msg) {
+
+                            }
+
+                            @Override
+                            public void debug(Marker marker, String format, Object arg) {
+
+                            }
+
+                            @Override
+                            public void debug(Marker marker, String format, Object arg1, Object arg2) {
+
+                            }
+
+                            @Override
+                            public void debug(Marker marker, String format, Object... arguments) {
+
+                            }
+
+                            @Override
+                            public void debug(Marker marker, String msg, Throwable t) {
+
+                            }
+
+                            @Override
+                            public boolean isInfoEnabled() {
+                                return false;
+                            }
+
+                            @Override
+                            public void info(String msg) {
+                                System.out.println("INFO:" + msg);
+                            }
+
+                            @Override
+                            public void info(String format, Object arg) {
+
+                            }
+
+                            @Override
+                            public void info(String format, Object arg1, Object arg2) {
+
+                            }
+
+                            @Override
+                            public void info(String format, Object... arguments) {
+
+                            }
+
+                            @Override
+                            public void info(String msg, Throwable t) {
+
+                            }
+
+                            @Override
+                            public boolean isInfoEnabled(Marker marker) {
+                                return false;
+                            }
+
+                            @Override
+                            public void info(Marker marker, String msg) {
+
+                            }
+
+                            @Override
+                            public void info(Marker marker, String format, Object arg) {
+
+                            }
+
+                            @Override
+                            public void info(Marker marker, String format, Object arg1, Object arg2) {
+
+                            }
+
+                            @Override
+                            public void info(Marker marker, String format, Object... arguments) {
+
+                            }
+
+                            @Override
+                            public void info(Marker marker, String msg, Throwable t) {
+
+                            }
+
+                            @Override
+                            public boolean isWarnEnabled() {
+                                return false;
+                            }
+
+                            @Override
+                            public void warn(String msg) {
+                                System.out.println("WARN:" + msg);
+
+                            }
+
+                            @Override
+                            public void warn(String format, Object arg) {
+
+                            }
+
+                            @Override
+                            public void warn(String format, Object... arguments) {
+
+                            }
+
+                            @Override
+                            public void warn(String format, Object arg1, Object arg2) {
+
+                            }
+
+                            @Override
+                            public void warn(String msg, Throwable t) {
+
+                            }
+
+                            @Override
+                            public boolean isWarnEnabled(Marker marker) {
+                                return false;
+                            }
+
+                            @Override
+                            public void warn(Marker marker, String msg) {
+
+                            }
+
+                            @Override
+                            public void warn(Marker marker, String format, Object arg) {
+
+                            }
+
+                            @Override
+                            public void warn(Marker marker, String format, Object arg1, Object arg2) {
+
+                            }
+
+                            @Override
+                            public void warn(Marker marker, String format, Object... arguments) {
+
+                            }
+
+                            @Override
+                            public void warn(Marker marker, String msg, Throwable t) {
+
+                            }
+
+                            @Override
+                            public boolean isErrorEnabled() {
+                                return false;
+                            }
+
+                            @Override
+                            public void error(String msg) {
+                                System.out.println("ERR:" + msg);
+
+                            }
+
+                            @Override
+                            public void error(String format, Object arg) {
+
+                            }
+
+                            @Override
+                            public void error(String format, Object arg1, Object arg2) {
+
+                            }
+
+                            @Override
+                            public void error(String format, Object... arguments) {
+
+                            }
+
+                            @Override
+                            public void error(String msg, Throwable t) {
+
+                            }
+
+                            @Override
+                            public boolean isErrorEnabled(Marker marker) {
+                                return false;
+                            }
+
+                            @Override
+                            public void error(Marker marker, String msg) {
+
+                            }
+
+                            @Override
+                            public void error(Marker marker, String format, Object arg) {
+
+                            }
+
+                            @Override
+                            public void error(Marker marker, String format, Object arg1, Object arg2) {
+
+                            }
+
+                            @Override
+                            public void error(Marker marker, String format, Object... arguments) {
+
+                            }
+
+                            @Override
+                            public void error(Marker marker, String msg, Throwable t) {
+
+                            }
+                        };
+                    }
                     logger.error("Unable to generate logger from class.", e);
                 } catch (Error e) {
                     logger = LoggerFactory.getLogger("com.optimizely.ab.android.sdk.OptimizelyManager");
