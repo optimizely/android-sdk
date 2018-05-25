c8690419ada260df12d00a3ffe515fa22556e43d
Thomas Zurkan <thomas.zurkan@optimizely.com>
2018-05-25 08:23:24 -0700

    add comments and license.

--- shared/src/main/java/com/optimizely/ab/android/shared/EnvironmentKey.java
+++ shared/src/main/java/com/optimizely/ab/android/shared/EnvironmentKey.java
@@ -1,7 +1,27 @@
+/****************************************************************************
+ * Copyright 2017, Optimizely, Inc. and contributors                        *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ *    http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ***************************************************************************/
+
 package com.optimizely.ab.android.shared;
 
 import android.support.annotation.NonNull;
 
+/**
+ * EnvironmentKey represents the environment.  Using this class you can get the
+ * environment datafile cache key along with the datafile url.
+ */
 public class EnvironmentKey implements ProjectKey {
     @NonNull private final String environmentKey;
 
@@ -9,6 +29,10 @@ public class EnvironmentKey implements ProjectKey {
         this.environmentKey = environmentKey;
     }
 
+    /**
+     * Get the cache key for this environment.
+     * @return the cache key for this environment.
+     */
     @Override
     public String getCacheKey() {
         String[] keys = environmentKey.split("/");
@@ -19,6 +43,10 @@ public class EnvironmentKey implements ProjectKey {
         return suffix;
     }
 
+    /**
+     * Get the url for this environment.
+     * @return the url for this environment.
+     */
     @Override
     public String getUrl() {
         return environmentKey;
--- shared/src/main/java/com/optimizely/ab/android/shared/ProjectId.java
+++ shared/src/main/java/com/optimizely/ab/android/shared/ProjectId.java
@@ -1,8 +1,31 @@
+/****************************************************************************
+ * Copyright 2017, Optimizely, Inc. and contributors                        *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ *    http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ***************************************************************************/
+
 package com.optimizely.ab.android.shared;
 
 import android.support.annotation.NonNull;
-import java.util.Arrays;
 
+/**
+ * A class to encapsulate the project id and any environment that might be used.
+ * It can create the cache key used if you want to clear the cache for that project or environment.
+ * It can also create the url depending on whether there is a environment or not.
+ * Generally, this class is under the hood.  However, it should be noted that if you are using background
+ * datafile updates, you need to make sure and call stop if you no longer want those updates.  For instance,
+ * if you change environments or projects and no longer want updates from the previous.
+ */
 public class ProjectId implements ProjectKey {
     public static String projectUrl = "https://cdn.optimizely.com/json/%s.json";
     public static String delimitor = "::::";
@@ -10,6 +33,12 @@ public class ProjectId implements ProjectKey {
     @NonNull private final String projectId;
     private EnvironmentKey environmentKey;
 
+    /**
+     * Constructor used to construct a ProjectId to get cache key, url,
+     * and environment.
+     * @param projectId project id string.
+     * @param environmentKey the envioronment url.
+     */
     public ProjectId(@NonNull String projectId, String environmentKey) {
         this.projectId = projectId;
         if (environmentKey != null) {
@@ -17,18 +46,37 @@ public class ProjectId implements ProjectKey {
         }
     }
 
+    /**
+     * Constructor with no environment
+     * @param projectId the current project id string.
+     */
     public ProjectId(@NonNull String projectId) {
         this(projectId, null);
     }
 
+    /**
+     * This returns the current project id string.
+     * @return project id string.
+     */
     public String getId() {
         return projectId;
     }
 
+    /**
+     * The environment key object for this project id.
+     * This can be null.
+     * @return environment object for current project config if any.
+     */
     public EnvironmentKey getEnvironmentKey() {
         return environmentKey;
     }
 
+    /**
+     * Return the cacheky for this project id. Or, return
+     * the environment cache key if there is an environment.
+     *
+     * @return cache key used to cache datafile.
+     */
     @Override
     public String getCacheKey() {
         if (environmentKey != null) {
@@ -38,6 +86,11 @@ public class ProjectId implements ProjectKey {
         return projectId;
     }
 
+    /**
+     * Get the url associated with this project.  If there is an environment,
+     * that url is returned.
+     * @return url of current project configuration.
+     */
     @Override
     public String getUrl() {
         if (environmentKey != null) {
@@ -48,6 +101,11 @@ public class ProjectId implements ProjectKey {
         }
     }
 
+    /**
+     * To string either returns the proejct id as string or a concatenated string of projectid
+     * delimitor and environment key.
+     * @return
+     */
     @Override
     public String toString() {
         return projectId + (environmentKey != null? delimitor + environmentKey.toString() : "");
