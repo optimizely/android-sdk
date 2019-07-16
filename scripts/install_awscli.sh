7f1ff34f78dde3e3be5fa29d68b10e0dc209dc7f
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-07-16 14:16:14 -0700

    a

--- scripts/install_awscli.sh
+++ scripts/install_awscli.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euox pipefail
 
 # requires Python 2 version 2.6.5+ or Python 3 version 3.3+
 
@@ -9,3 +9,4 @@ unzip "$WORKDIR/awscli-bundle.zip" -d "$WORKDIR"
 sudo "$WORKDIR/awscli-bundle/install" -i /usr/local/aws -b /usr/local/bin/aws
 rm -rf "$WORKDIR/awscli-bundle"
 rm "$WORKDIR/awscli-bundle.zip"
+echo "$0 finished execution"
