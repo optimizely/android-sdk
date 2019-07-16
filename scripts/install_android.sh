fc983173b66c4286d82cbb5742d3b02e62cbe2e8
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-07-16 13:35:23 -0700

    cleanup

---
+++ scripts/install_android.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+wget -q "https://dl.google.com/android/repository/sdk-tools-linux-$SDK_TOOLS.zip" -O android-sdk-tools.zip && unzip -q android-sdk-tools.zip -d "$ANDROID_HOME" && rm android-sdk-tools.zip
+mkdir -p ~/.android && touch ~/.android/repositories.cfg
+yes | sdkmanager --licenses > /dev/null
+sdkmanager "emulator" "tools" "platform-tools" > /dev/null
+sdkmanager "build-tools;$ANDROID_BUILD_TOOLS" "platforms;android-$ANDROID_API" > /dev/null
+sdkmanager "system-images;android-$EMULATOR_API;google_apis;armeabi-v7a" > /dev/null
+sdkmanager "extras;google;m2repository" "extras;android;m2repository" "add-ons;addon-google_apis-google-19" > /dev/null
+sdkmanager --list | head -15
+echo no | avdmanager create avd -n test -k "system-images;android-$EMULATOR_API;google_apis;armeabi-v7a"
+$ANDROID_HOME/emulator/emulator -avd test -no-audio -no-window &
+scripts/android-wait-for-emulator.sh
+adb shell input keyevent 82
+echo "$0 finished execution"
---
+++ scripts/install_java.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# this script removes any existing java that comes with a travis worker and installs our own
+
+
+function cleanup_ {
+  # remove all java versions that come with worker image
+  sudo apt-get -y remove default-jdk default-jre default-jdk-headless default-jre-headless ca-certificates-java openjdk-8-jdk openjdk-8-jdk-headless openjdk-8-jre openjdk-8-jre-headless
+  # just because we are paranoid, lets make sure we get them all
+  dpkg-query -f '${Package} ${Status}\n' -W "openjdk*" | grep 'ok installed' | awk '{print $1}' | xargs sudo apt-get -y remove
+  # remove java custom installed by travis since $PATH looks for this first before /usr/lib/jvm
+  sudo rm -rf /usr/local/lib/jvm
+}
+
+function install_ {
+  TARBALL=jdk-8u211-linux-x64.tar.gz
+  WORKDIR=/tmp/workdir
+  mkdir -p $WORKDIR
+  # fetch manually downloaded tarbal of oracle's jdk 8
+  aws s3 cp "s3://optly-fs-travisci-artifacts/java/$TARBALL" "$WORKDIR/"
+  # get java installer script
+  wget -O $WORKDIR/install-java.sh https://raw.githubusercontent.com/juancarlostong/install-java/master/install-java.sh
+  chmod u+x $WORKDIR/install-java.sh
+  # install
+  yes | $WORKDIR/install-java.sh -f $WORKDIR/$TARBALL
+  # set JAVA_HOME
+  source ~/.bashrc
+}
+
+function verify_ {
+  echo "JAVA_HOME=$JAVA_HOME"
+  which java
+  java -version
+  echo "$0 finished execution"
+}
+
+function main {
+  cleanup_
+  install_
+  verify_
+}
+main
