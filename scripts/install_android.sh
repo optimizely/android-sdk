9e074356970401648d77c398c5e61a2d6654ed66
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-07-16 16:24:52 -0700

    a

--- scripts/install_android.sh
+++ scripts/install_android.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-IFS=$'\n\t'
 
 WORKDIR=/tmp/workdir
 mkdir -p "$WORKDIR"
 wget -q "https://dl.google.com/android/repository/sdk-tools-linux-$SDK_TOOLS.zip" -O "$WORKDIR/android-sdk-tools.zip" && unzip -q "$WORKDIR/android-sdk-tools.zip" -d "$ANDROID_HOME" && rm "$WORKDIR/android-sdk-tools.zip"
 mkdir -p ~/.android && touch ~/.android/repositories.cfg
 yes | sdkmanager --licenses > /dev/null
-sdkmanager "emulator" "tools" "platform-tools" > /dev/null
+sdkmanager "emulator" "tools" "platform-tools"
 sdkmanager "build-tools;$ANDROID_BUILD_TOOLS" "platforms;android-$ANDROID_API" > /dev/null
 sdkmanager "system-images;android-$EMULATOR_API;google_apis;armeabi-v7a" > /dev/null
 sdkmanager "extras;google;m2repository" "extras;android;m2repository" "add-ons;addon-google_apis-google-19" > /dev/null
