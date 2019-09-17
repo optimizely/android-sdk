2704849a91ebdd58d0369717074e9bc6b8aada97
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-09-17 18:41:55 +0500

    updated travis script

--- scripts/get-fsc-tests.sh
+++ scripts/get-fsc-tests.sh
@@ -4,12 +4,9 @@ set -e
 FSC_PATH=tmp/fsc-repo
 rm -rf $FSC_PATH
 mkdir -p $FSC_PATH
-test -n "$FSC_PATH" && if [ "$FSC_PATH" != "git" ]; then
-  exit 0
-fi
  
 pushd $FSC_PATH && git init && git fetch --depth=1 https://$CI_USER_TOKEN@github.com/optimizely/fullstack-sdk-compatibility-suite ${FSC_BRANCH:-master} && git checkout FETCH_HEAD && popd
-cp ./$FSC_PATH/features/support/datafiles ./test-app/src/main/res/raw
-cp ./$FSC_PATH/features/ ./test-app/src/androidTest/assets/features
+cp -r ./$FSC_PATH/features/support/datafiles/. ./test-app/src/main/res/raw/
+cp -r ./$FSC_PATH/features/. ./test-app/src/androidTest/assets/features/
 
 echo "Ready for testing."
