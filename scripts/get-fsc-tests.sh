f2c60c62f309ad3266bc31eeb7aa1a1660c93614
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-09-17 22:13:39 +0500

    logging fsc

--- scripts/get-fsc-tests.sh
+++ scripts/get-fsc-tests.sh
@@ -6,8 +6,12 @@ rm -rf $FSC_PATH
 mkdir -p $FSC_PATH
  
 pushd $FSC_PATH && git init && git fetch --depth=1 https://$CI_USER_TOKEN@github.com/optimizely/fullstack-sdk-compatibility-suite ${FSC_BRANCH:-master} && git checkout FETCH_HEAD && popd
+ls ./
 cp -r ./$FSC_PATH/features/support/datafiles/*.json ./test-app/src/main/res/raw/
 cp -r ./$FSC_PATH/features ./test-app/src/androidTest/assets/
-
+ls $FSC_PATH
+ls ./$FSC_PATH
+ls ./$FSC_PATH/features/support/datafiles/*.json
+ls ./test-app/src/main/res/raw/
 ls ./test-app/src/androidTest/assets/features
 echo "Ready for testing."
