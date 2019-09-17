4c600ee08df3de34bb1edab3a4024a49f51214cf
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-09-17 21:55:15 +0500

    logging for output

--- scripts/get-fsc-tests.sh
+++ scripts/get-fsc-tests.sh
@@ -9,4 +9,5 @@ pushd $FSC_PATH && git init && git fetch --depth=1 https://$CI_USER_TOKEN@github
 cp -r ./$FSC_PATH/features/support/datafiles/*.json ./test-app/src/main/res/raw/
 cp -r ./$FSC_PATH/features ./test-app/src/androidTest/assets/
 
+ls ./test-app/src/androidTest/assets/features
 echo "Ready for testing."
