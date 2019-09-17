02e73fdbda437cb229389b80b0a38195d7cc0978
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-09-17 21:23:25 +0500

    copying directory

--- scripts/get-fsc-tests.sh
+++ scripts/get-fsc-tests.sh
@@ -7,6 +7,6 @@ mkdir -p $FSC_PATH
  
 pushd $FSC_PATH && git init && git fetch --depth=1 https://$CI_USER_TOKEN@github.com/optimizely/fullstack-sdk-compatibility-suite ${FSC_BRANCH:-master} && git checkout FETCH_HEAD && popd
 cp -r ./$FSC_PATH/features/support/datafiles/*.json ./test-app/src/main/res/raw/
-cp -r ./$FSC_PATH/features/. ./test-app/src/androidTest/assets/features/
+cp -r ./$FSC_PATH/features ./test-app/src/androidTest/assets/
 
 echo "Ready for testing."
