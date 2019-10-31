4d4dcdfaa9c944cfbbcddac7fbf1aef46a030272
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-10-31 19:12:22 +0500

    updated get-fsc-tests script and made it generic this script can be put into travis-ci tools

--- scripts/get-fsc-tests.sh
+++ scripts/get-fsc-tests.sh
@@ -1,14 +1,71 @@
 #!/bin/bash
-# This script fetches Full stack compatibility suite and copy feature files in to fsc-app assets directory
-# and copy datafiles from Full stack compatibility suite in to raw datafiles folder of fsc-app module
-set -e
-FSC_PATH=tmp/fsc-repo
-rm -rf $FSC_PATH
-mkdir -p $FSC_PATH
- 
-pushd $FSC_PATH && git init && git fetch --depth=1 https://$CI_USER_TOKEN@github.com/optimizely/fullstack-sdk-compatibility-suite ${FSC_BRANCH:-master} && git checkout FETCH_HEAD && popd
-mkdir -p ./fsc-app/src/main/res/raw
-cp -r ./$FSC_PATH/features/support/datafiles/*.json ./fsc-app/src/main/res/raw/
-mkdir -p ./fsc-app/src/androidTest/assets/features
-cp -r ./$FSC_PATH/features/* ./fsc-app/src/androidTest/assets/features/
+
+# This script fetches Full stack compatibility suite and copies all the feature files and datafiles to the given paths.
+
+# inputs:
+# FEATURES_PATH - destination path to copy feature files (required)
+# DATAFILES_PATH - destination path to use for datafiles (required)
+# TAG_FILTER_EXPRESSION - Gherkin filter
+FEATURE_FILES_PATH="${FEATURES_PATH:-}"
+DATAFILES_PATH="${DATAFILES_PATH:-}"
+TAG_FILTER_EXPRESSION=""
+usage() { echo "Usage: $0 -h [-t <string>] [-f <string>] [-d <string>]" 1>&2; }
+
+show_example() { cat <<EOF
+Example: $0 -f /usr/tests/integration/features -d /usr/support/fsc-datafiles -t "@FEATURE_ROLLOUT && ~@INPUT_FILTER"
+EOF
+}
+
+while getopts ":t:f:d:h" o; do
+  case "${o}" in    
+    f)
+      FEATURE_FILES_PATH=${OPTARG}
+      ;;
+    d)
+      DATAFILES_PATH=${OPTARG}
+      ;;
+    t)
+      TAG_FILTER_EXPRESSION=${OPTARG}
+      ;;
+    h)
+      usage
+      echo
+      show_example
+      exit 1
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
+  esac
+
+done
+shift $((OPTIND-1))
+
+
+if [ -z "$FEATURE_FILES_PATH" ]; then
+  echo
+  echo "-f is a required argument"
+  echo
+  show_example
+  exit 1
+fi
+
+if [ -z "$DATAFILES_PATH" ]; then
+  echo
+  echo "-d is a required argument"
+  echo
+  show_example
+  exit 1
+fi
+
+set -ex
+FSC_FEATURE_FILES_PATH="$FSC_PATH/features/"
+rm -rf $FEATURE_FILES_PATH
+mkdir -p $FEATURE_FILES_PATH
+cp -r $FSC_FEATURE_FILES_PATH/* $FEATURE_FILES_PATH
+
+FSC_DATAFILES_DIR="$FSC_PATH/features/support/datafiles"
+mkdir -p $DATAFILES_PATH
+cp -r $FSC_DATAFILES_DIR/*.json "$DATAFILES_PATH"
 echo "Ready for testing."
