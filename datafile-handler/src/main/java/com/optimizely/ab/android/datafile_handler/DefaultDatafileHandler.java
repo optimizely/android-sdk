1672dd92e4845e022188aa59cf16935a55ced9ea
Tom Zurkan <thomas.zurkan@optimizely.com>
2020-06-15 12:33:38 -0700

    remove unused import

--- datafile-handler/src/main/java/com/optimizely/ab/android/datafile_handler/DefaultDatafileHandler.java
+++ datafile-handler/src/main/java/com/optimizely/ab/android/datafile_handler/DefaultDatafileHandler.java
@@ -38,7 +38,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * The default implementation of {@link DatafileHandler} and the main
