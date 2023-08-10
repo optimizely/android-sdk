d52b3fa761b58b03cbc43fc3c44182154377ba84
Muhammad Noman <Muhammadnoman@folio3.com>
2023-08-10 19:56:20 +0500

    Update OptimizelyClient.java

--- android-sdk/src/main/java/com/optimizely/ab/android/sdk/OptimizelyClient.java
+++ android-sdk/src/main/java/com/optimizely/ab/android/sdk/OptimizelyClient.java
@@ -14,6 +14,7 @@
  * limitations under the License.                                           *
  ***************************************************************************/
 
+// Testing
 package com.optimizely.ab.android.sdk;
 
 import android.content.Context;
